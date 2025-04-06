fceba5a927e85a146572446c46b52fadca3314b1
EmperorPalkkitine <spalkki@asu.edu>
2025-04-07 00:02:04 +0300

    maybe fix fetch method?

--- app.py
+++ app.py
@@ -132,9 +132,6 @@ def get_data(equipment_id):
         cursor.execute(query, (equipment_id.strip(),))
         print(f"Executing query: {query}, with equipment_id: {equipment_id}")  # Debug print
         
-        # Make sure any previous results are cleared
-        cursor.fetchall()  # Clear any remaining results
-        
         result = cursor.fetchone()
 
         if not result:
